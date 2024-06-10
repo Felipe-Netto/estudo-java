99dae2282e79701b09c9b642ccf080aefe84d01b
Felipe-Netto <felipeg.alvesnetto@gmail.com>
2024-06-10 00:59:32 -0300

    Estudo sobre inputs com args e scanner

---
+++ terminal/src/AboutMeScanner.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AboutMeScanner {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + "cm");
+
+        scanner.close();
+
+    }
+}
