46af4731bbb94b30e9dc65fc0ad9127ebc7b3106
Felipe-Netto <felipeg.alvesnetto@gmail.com>
2024-06-10 16:26:09 -0300

    Adicionando Tratamento de Exceções

--- terminal/src/AboutMeScanner.java
+++ terminal/src/AboutMeScanner.java
@@ -1,8 +1,12 @@
+import java.util.InputMismatchException;
+import java.util.Locale; // serve para utilizar algum padrão de linguagem, neste caso, o americano
 import java.util.Scanner;
 
 public class AboutMeScanner {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+
+        try{
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.print("Digite seu nome: ");
         String nome = scanner.next();
@@ -18,9 +22,13 @@ public class AboutMeScanner {
 
         System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos");
-        System.out.println("Minha altura é " + altura + "cm");
+        System.out.println("Minha altura é " + altura + "m");
 
         scanner.close();
+        }
+        catch(InputMismatchException e){
+            System.err.println("Os campos idade e altura devem ser numéricos");
+        }
 
     }
 }
