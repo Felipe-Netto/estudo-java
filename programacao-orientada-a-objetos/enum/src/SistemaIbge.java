602543f1dbe41326f13d63cd068bcb351ac2af0d
Felipe-Netto <felipeg.alvesnetto@gmail.com>
2024-06-10 23:33:17 -0300

    Aprendendo sobre enums

---
+++ programacao-orientada-a-objetos/enum/src/SistemaIbge.java
@@ -0,0 +1,11 @@
+public class SistemaIbge {
+    public static void main(String[] args) {
+        for(EstadoBrasileiro e: EstadoBrasileiro.values()){
+            System.out.println(e.getNome() + " - " + e.getSigla());
+        }
+
+        EstadoBrasileiro eb = EstadoBrasileiro.SAO_PAULO;
+
+        System.out.println(eb.getNome() + " - " + eb.getSigla());
+    }
+}
