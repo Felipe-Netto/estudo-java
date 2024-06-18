deef99968115a55868d3fa54087c01963c578143
Felipe-Netto <felipeg.alvesnetto@gmail.com>
2024-06-17 22:30:42 -0300

    tarefaConcluida inicia false pelo construtor
    Signed-off-by: Felipe-Netto <felipeg.alvesnetto@gmail.com>

--- collections-java/set/src/Pesquisa/ListaTarefas/Tarefa.java
+++ collections-java/set/src/Pesquisa/ListaTarefas/Tarefa.java
@@ -2,10 +2,11 @@ package Pesquisa.ListaTarefas;
 
 public class Tarefa {
     private String descricao;
-    private boolean tarefaConcluida = false;
+    private boolean tarefaConcluida;
 
     public Tarefa(String descricao){
         this.descricao = descricao;
+        this.tarefaConcluida = false;
     }
 
     public String getDescricao(){
